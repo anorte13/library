86dc8e8950b4b693c3105f14e24b1c473a588ac9
Angel <angel-ortega27@hotmail.com>
2022-08-25 17:31:06 -0700

    update addToLibrary function, added UI elements

--- library-logic.js
+++ library-logic.js
@@ -1,27 +1,27 @@
 const form = document.getElementById('bookform');
+let error = document.getElementById('error');
+
+
 const title = form.elements['booktitle'];
 const author = form.elements['authorname'];
 const pages = form.elements['pages'];
+const read = form.elements['status']
+
+
 
 let bookTitle;
 let authorName;
 let numberPages;
-let myBooks;
+let readStatus;
 let myLibrary = [];
+let bookInfo;
 
-
-function Book (title, author, pages) {
+function Book (title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
+    this.read = read;
 }
-
-Book.prototype.bookInfo = function () {
-    let bookData = this.title + ' by ' + this.author + ', ' + this.pages + ' pages';
-    console.log(bookData);
-}
-
-
 function inputForm() {
     if(form.style.display === 'block') {
         form.style.display = "none";
@@ -33,11 +33,34 @@ function addBookToLibrary() {
     bookTitle = title.value;
     authorName = author.value;
     numberPages = pages.value;
-    const myBook = new Book(bookTitle, authorName, numberPages);
-    myBooks = myBook;
-;  
-}
-function showLibrary() {
-    console.log(typeof myBooks);
+
+    if (read.checked) {
+        readStatus = 'Read';
+    }
+    else {
+        readStatus = 'Not Read';
+    }
+    let myBook = new Book(bookTitle, authorName, numberPages, readStatus);
+    myLibrary.push(myBook);
+    bookAdded();
+    showLibrary();
 }
 
+function bookAdded() {
+    let msg = document.getElementById("demo");
+    msg.textContent = 'Your book was added to your library!';
+    msg.style.color = 'green';
+}
+function showLibrary() {
+    if(myLibrary.length === 0) {
+        error.textContent = 'There are no books in library, please add some';
+    }
+    else {
+        for (let i = 0; i < myLibrary.length; i++) {
+            error.textContent = myLibrary[i].title + ' by ' + myLibrary[i].author + ', ' + myLibrary[i].pages + ' pages ' + myLibrary[i].read;
+        }
+    }
+}
+function clearForm() {
+    form.reset();
+}
